81868c20ab86492f2131c1c882386f7f4e403c78
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-03-10 22:04:30 -0700

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 //Key into the server
 var api_key = "b709b8b1255dbfd93b409d2ca2ece655"
+var historyEl=document.querySelector("#history");
+var weatherHistory=JSON.parse(localStorage.getItem("city-name")) || [];
+console.log(weatherHistory);
+
 
 //function takes city name and retrieves weather data for that city
 function getWeather(city){
@@ -29,12 +33,34 @@ fetch(onecallURL)
 }));
 
 });
+
+
+var currentDate= new Date();
+console.log(currentDate.toDateString());
+
+var currentTemperature= document.querySelector("#temp");
+var currentHumidity= document.querySelector("#humidity");
+var currentUvi=document.querySelector("#uvi");
+var currentCity=document.querySelector("#currentCity");
+
+currentCity.innerHTML= "hello " + currentDate; 
+
+function getFahrenheit(K) {
+    return Math.floor((K - 273.15) *1.8 +32);
+}
+currentTemperature.innerHTML = " Temperature: " + getFahrenheit(oncecallData.main.temp);
+var addCity= document.querySelector("#add-city") 
 }
 
-getWeather("tucson");
+// var search=document.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     if(!e.target.matches("city-name")) return;
 
-search.addEventListener("submit', function(e){
-e.preventDefault()
-})
-var city= document.querySelector(".form-control").value
-var form= document.querySelector("search")
+//     var city= e.target.textContent;
+
+        //   getWeather(city);
+        // });
+
+
+getWeather("phoenix");
+
