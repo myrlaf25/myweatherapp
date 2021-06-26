bf00db07cec14d8f2434e4a60b43639855bc01d2
myrla <myrlaf25@gmail.com>
2021-06-25 20:49:03 -0700

    added icon and desc

--- assets/script.js
+++ assets/script.js
@@ -4,31 +4,25 @@ var api_key = "b709b8b1255dbfd93b409d2ca2ece655";
 var historyEl = document.querySelector("#history");
 var inputEl = document.querySelector("#city-name");
 var currentCity = document.querySelector("#currentCity");
-var searchedCities= document.querySelector("#searchedCities");
+var searchedCities = document.querySelector("#searchedCities");
 
-searchedCities.addEventListener("click", function(e){
-    if (!e.target.matches("p")) {
-      return
-    }
-    var cityName = e.target.textContent;
-    getWeather(cityName);
-  });
+searchedCities.addEventListener("click", function (e) {
+  if (!e.target.matches("p")) {
+    return;
+  }
+  var cityName = e.target.textContent;
+  getWeather(cityName);
+});
 
-
-
-//this function is to show the cities the used has been searching for 
-function renderSearchedCities(){
-    searchedCities.innerHTML="";
-    for(i=0; i< weatherHistory.length; i++){
-        var searchedEl = document.createElement('p');
-        searchedEl.textContent=weatherHistory[i];
-        searchedCities.append(searchedEl)
-        
-    }
-
-
+//this function is to show the cities the used has been searching for
+function renderSearchedCities() {
+  searchedCities.innerHTML = "";
+  for (i = 0; i < weatherHistory.length; i++) {
+    var searchedEl = document.createElement("p");
+    searchedEl.textContent = weatherHistory[i];
+    searchedCities.append(searchedEl);
+  }
 }
-
 
 //this function is to convert the temperature the api is providing in kelvin to change to fahrenheit
 function getFahrenheit(K) {
@@ -43,14 +37,14 @@ function getWeather(city) {
     .then((data) => data.json())
     .then(function (weather) {
       console.log(weather);
-//if the city cannot be found it will alert with a window pop up to the user it may not be found
+      //if the city cannot be found it will alert with a window pop up to the user it may not be found
       if (weather.cod === "404") {
         alert("City not found");
         return;
       }
-//thsi helped fix a bow of duplication
+      //thsi helped fix a bow of duplication
       currentCity.innerHTML = "" + weather.name;
-//create variables to pull the  latitue and longitude from the api info provided
+      //create variables to pull the  latitue and longitude from the api info provided
       var lat = weather.coord.lat;
       var lon = weather.coord.lon;
 
@@ -63,118 +57,107 @@ function getWeather(city) {
           buildDashboard(onecallData);
         });
     });
-    //this function is to get teh current date, temperature, humidity, and UV
-    function buildDashboard(data) {
-        var currentDate = new Date(data.current.dt * 1000);
-        console.log(currentDate);
-        var day = currentDate.getDate();
-        var month = currentDate.getMonth() + 1;
-        var year = currentDate.getFullYear();
-        
-  var currentTemperature = document.querySelector("#temp");
-  var currentHumidity = document.querySelector("#humidity");
-  var currentUvi = document.querySelector("#uvi");
-  var searchBtn = document.querySelector("#searchBtn");
-  var clearHistoryBtn = document.querySelector("#clearHistoryBtn");
-  
-  currentCity.innerHTML += " " + month + "/" + day + "/" + year + " ";
-  currentTemperature.innerHTML =
-  "Temperature: " + getFahrenheit(data.current.temp) + " F";
-  currentHumidity.innerHTML = "Humidity: " + data.current.humidity + " %";
+  //this function is to get teh current date, temperature, humidity, and UV
+  function buildDashboard(data) {
+    var currentDate = new Date(data.current.dt * 1000);
+    console.log(currentDate);
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
 
-  currentUvi.innerHTML = "UV Index: " + data.current.uvi;
-  
-  // uviIndex = document.getElementById('range');
-  
-  function findUviIndex (uviIndex){
-    var uviIndex=data.current.uvi;
-    console.log(uviIndex)
-    
-    if(uviIndex <= 2.00) {
-      document.getElementById('range').innerHTML='Index is Low.';
-    } else if (uviIndex >=3.00 && uviIndex <=5.00){
-      document.getElementById('range').innerHTML='Index is Moderate.';
-    } else {
-      document.getElementById('range').innerHTML='Index is High.';
+    var currentTemperature = document.querySelector("#temp");
+    var currentHumidity = document.querySelector("#humidity");
+    var currentUvi = document.querySelector("#uvi");
+    var searchBtn = document.querySelector("#searchBtn");
+    var clearHistoryBtn = document.querySelector("#clearHistoryBtn");
+
+    currentCity.innerHTML += " " + month + "/" + day + "/" + year + " ";
+    currentTemperature.innerHTML =
+      "Temperature: " + getFahrenheit(data.current.temp) + " F";
+    currentHumidity.innerHTML = "Humidity: " + data.current.humidity + " %";
+
+    currentUvi.innerHTML = "UV Index: " + data.current.uvi;
+
+    // uviIndex = document.getElementById('range');
+
+    function findUviIndex(uviIndex) {
+      var uviIndex = data.current.uvi;
+      console.log(uviIndex);
+
+      if (uviIndex <= 2.0) {
+        document.getElementById("range").innerHTML = "Index is Low.";
+      } else if (uviIndex >= 3.0 && uviIndex <= 5.0) {
+        document.getElementById("range").innerHTML = "Index is Moderate.";
+      } else {
+        document.getElementById("range").innerHTML = "Index is High.";
+      }
     }
-  }
-  findUviIndex();
+    findUviIndex();
 
-  let icon = data.current.weather[0].icon;
- var currentIcon = document.createElement('img');
-
-console.log(icon);
- currentIcon.setAttribute('src', 'http://openweathermap.org/img/wn/' + icon + '@2x.png');
-
-
-
-
-
-  var cityForecast=document.querySelector("#forecast");
+    let icon = data.current.weather[0].icon;
+    var currentIcon = document.querySelector("#icon");
+    console.log(icon);
+    currentIcon.setAttribute(
+      "src",
+      "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+    );
   
-  
-  
-  //this prevents duplication in the for loop when displaying the 5 day forecast
-  cityForecast.innerHTML="";
-  // thsi is a for loop to pull the array info from the one provided from the api up to index 5
-  for(i=1; i < 6; i++){
-      var forecastIndex = data.daily[i];  
+    currentIcon.append(icon);
+
+    let weatherDesc = data.current.weather[0].description;
+    console.log(weatherDesc);
+    var currentDesc = document.querySelector("#description");
+    currentDesc.innerHTML = weatherDesc;
+
+    var cityForecast = document.querySelector("#forecast");
+
+    //this prevents duplication in the for loop when displaying the 5 day forecast
+    cityForecast.innerHTML = "";
+    // thsi is a for loop to pull the array info from the one provided from the api up to index 5
+    for (i = 1; i < 6; i++) {
+      var forecastIndex = data.daily[i];
       console.log(forecastIndex);
-      cityForecast.append(buildForecast(forecastIndex))
+      cityForecast.append(buildForecast(forecastIndex));
     }
     //this function is to get teh 5 day forecast for the city in the search input
-    function buildForecast(forecast){
-        
-        var col =document.createElement("div");
-        col.classList.add("col");
-        
-        var forecastContainer =document.createElement("div");
-        forecastContainer.classList.add("big-primary", "rounded", "p-5");
-        //if I have time I will try to pull an icon for the weather display
-        // var img = document.createElement("img");
-        // img.setAttribute("src", forecast.url);
-        
-        
-        
-        
-        
-        var forecastDate = new Date(data.daily[i].dt*1000);
-        console.log(forecastDate);
-        var forecastDay = forecastDate.getDate();
-        var forecastMonth = forecastDate.getMonth() + 1;
-        var forecastYear = forecastDate.getFullYear();
-        var forecastDate=document.createElement("h4");
-        forecastDate.textContent= forecast.forecastDate;
-        
-       
-        forecastDate.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
-      
-        
-        
-        
-        
-        
-        var forecastTemp = document.createElement("p");
-       
-        forecastTemp.innerHTML = "Temp: " + getFahrenheit(data.daily[i].temp.day)  + " F";
-        console.log(data.daily[i].temp);
-        console.log(getFahrenheit(data.daily[i].temp.day));
-        
-        
-        
-              
-        var forecastHumidity = document.createElement("p");
-        forecastHumidity.innerHTML = "Humidity: " + (data.daily[i].humidity) + "%";
-        
-        forecastContainer.append(forecastDate, forecastTemp, forecastHumidity);
-        
-       
+    function buildForecast(forecast) {
+      var col = document.createElement("div");
+      col.classList.add("col");
 
-        col.append(forecastContainer);
-        
-        return col;      
+      var forecastContainer = document.createElement("div");
+      forecastContainer.classList.add("big-primary", "rounded", "p-5");
+      //if I have time I will try to pull an icon for the weather display
+      // var img = document.createElement("img");
+      // img.setAttribute("src", forecast.url);
+
+      var forecastDate = new Date(data.daily[i].dt * 1000);
+      console.log(forecastDate);
+      var forecastDay = forecastDate.getDate();
+      var forecastMonth = forecastDate.getMonth() + 1;
+      var forecastYear = forecastDate.getFullYear();
+      var forecastDate = document.createElement("h4");
+      forecastDate.textContent = forecast.forecastDate;
+
+      forecastDate.innerHTML =
+        forecastMonth + "/" + forecastDay + "/" + forecastYear;
+
+      var forecastTemp = document.createElement("p");
+
+      forecastTemp.innerHTML =
+        "Temp: " + getFahrenheit(data.daily[i].temp.day) + " F";
+      console.log(data.daily[i].temp);
+      console.log(getFahrenheit(data.daily[i].temp.day));
+
+      var forecastHumidity = document.createElement("p");
+      forecastHumidity.innerHTML = "Humidity: " + data.daily[i].humidity + "%";
+
+      forecastContainer.append(forecastDate, forecastTemp, forecastHumidity);
+
+      col.append(forecastContainer);
+
+      return col;
     }
-    }             
+  }
 }
 
 // }
@@ -183,25 +166,18 @@ console.log(weatherHistory);
 
 renderSearchedCities();
 
-
 searchBtn.addEventListener("click", function (e) {
   var searchCity = inputEl.value;
   getWeather(searchCity);
   weatherHistory.push(searchCity);
-  
+
   renderSearchedCities();
 
-
   localStorage.setItem("search", JSON.stringify(weatherHistory));
-  
 });
 
-clearHistoryBtn.addEventListener("click", function(){
-    localStorage.clear();
-    weatherHistory=[];
-    searchedCities.innerHTML="";
-
-}) 
-
-
-
+clearHistoryBtn.addEventListener("click", function () {
+  localStorage.clear();
+  weatherHistory = [];
+  searchedCities.innerHTML = "";
+});
